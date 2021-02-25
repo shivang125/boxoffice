14d775e9209ccfb9a1f32f01bc642b25ea31a7a8
shivang <smshiv998@gmail.com>
2021-02-25 14:27:18 +0530

    edt react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react"
-
+import {Switch,Route} from 'react-router-dom'
 function App() {
   return (
-    <div >
-      hi
-    </div>
+    <Switch>
+     <Route exact={true} path="/">
+    This is home page
+
+    </Route>
+
+    <Route exact={true} path="/starred">
+    This is home starred
+
+    </Route>
+      </Switch>
   );
 }
 
