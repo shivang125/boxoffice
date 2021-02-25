241299a00663ac2319029fbdd7e05f0c887b2ea6
shivang <smshiv998@gmail.com>
2021-02-25 22:23:09 +0530

    created fetch and conn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+const [input,setInput] = useState('');
+const onInputChange =(ev) =>{
+  setInput(ev.target.value)
+};
+const onSearch=()=>{
+  //https://api.tvmaze.com/search/shows?q=men
+fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res=>res.json()).then(result =>{console.log(result);})
+};
+
+const onKeyDown=(ev)=>{
+if(ev.keyCode === 13)
+{
+ onSearch(); 
+}
+}
+
   return (
     <MainPageLayout>
-      this is home
+      <input type="text" onChange = {onInputChange} onKeyDown={onKeyDown} value={input}/>
+      <button type='button' onClick={onSearch}>Search</button>
     </MainPageLayout>
   )
 }
