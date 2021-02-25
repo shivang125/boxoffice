3c01df68db0cc14614eb1b6278b791d0095d4365
shivang <smshiv998@gmail.com>
2021-02-25 22:44:57 +0530

    Display shows results and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,59 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-const [input,setInput] = useState('');
-const onInputChange =(ev) =>{
-  setInput(ev.target.value)
-};
-const onSearch=()=>{
-  //https://api.tvmaze.com/search/shows?q=men
-fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res=>res.json()).then(result =>{console.log(result);})
-};
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
-const onKeyDown=(ev)=>{
-if(ev.keyCode === 13)
-{
- onSearch(); 
-}
-}
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <MainPageLayout>
-      <input type="text" onChange = {onInputChange} onKeyDown={onKeyDown} value={input}/>
-      <button type='button' onClick={onSearch}>Search</button>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
