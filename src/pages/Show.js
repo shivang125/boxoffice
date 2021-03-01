2b708753b1ef458093a624176fec9b0d4c2781a6
shivang <smshiv998@gmail.com>
2021-03-01 23:56:49 +0530

    created show components

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,9 @@
 import React,{useEffect,useReducer} from 'react'
 import {useParams} from "react-router-dom"
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer =(prevState,action)=>{
@@ -63,9 +67,25 @@ if (isLoading) {
 if (error) {
   return<div>error occured in loading</div>
 }
+
   return (
     <div>
-      this is show page
+      <ShowMainData image = {show.image} name={show.name} rating={show.rating} summary = {show.summary} tags={show.genres}/>
+
+      <div>
+        <h2>Details</h2>
+        <Details status={show.status} network={show.network} premiered={show.premiered}/>
+      </div>
+      
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons ={show._embedded.seasons}/>
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast}/>
+      </div>
     </div>
   )
 }
