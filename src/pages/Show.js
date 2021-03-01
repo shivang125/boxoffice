4b05297a7ddca431eb501d4b51ef3c0edb8017d9
shivang <smshiv998@gmail.com>
2021-03-01 23:31:18 +0530

    replaced useState with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,36 +1,63 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useReducer} from 'react'
 import {useParams} from "react-router-dom"
 import { apiGet } from '../misc/config';
+
+const reducer =(prevState,action)=>{
+  switch(action.type)
+  {
+    case 'FETCH_SUCCESS':{
+      return{isLoading:false,error : null,show:action.show}
+    }
+
+    case 'FETCH_FAILED':{
+      return {...prevState, isLoading:false,error:action.error}
+    }
+
+    default : return prevState
+  }
+}
+
+const initialState = {
+  show: null,
+  isLoading:true,
+  error:null
+}
 
 const Show = () => {
 
   const {id} = useParams();
 
-  const [show, setShow] = useState(null);
-  const [isLoading ,setIsLoading] = useState(true);
-  const [error , setError] = useState(null);
+  const [{show,isLoading,error},dispatch] = useReducer(reducer, initialState)
+
+
 
   useEffect(()=>{
-    apiGet(`/shows/{id}?embed[]=seasons&embed[]=cast`)
+
+    let isMounted= true;
+
+    apiGet(`/shows/{id}?embed[]=seasons&embed[]=cast`)//
     .then(results=>{
+      if (isMounted) {
+        dispatch({type:'FETCH_SUCCESS',show:results})
+      }
 
-      setTimeout(()=>{
-        setShow(results);
-        setIsLoading(false);
-      },2000)
-
-      
-    })
+    })   
     .catch(err =>{
-      setError(err.message);
-    setIsLoading(false);
-  })
+      if (isMounted) {
+        dispatch({type:'FETCH_FAILED',error:error.message})
+      }
+    })
+
+    return()=>{
+      isMounted = false;
+    }
+
   },[id])
 
 console.log("show",show);
 
 if (isLoading) {
-  return <div>Date is being loaded</div>
+  return <div>Data is being loaded</div>
 }
 
 if (error) {
