4a34b5da078b4c0761fa2115325951b39390e945
shivang <smshiv998@gmail.com>
2021-03-02 21:46:27 +0530

    Updates

---
+++ precache-manifest.8fcabdadaa911b42c1bce642a96c304b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3548376613f3841bc99f010a2e4359e4",
+    "url": "/boxoffice/index.html"
+  },
+  {
+    "revision": "8a68841e8bdd95db5b3f",
+    "url": "/boxoffice/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "8a0309596fc93fd5f8af",
+    "url": "/boxoffice/static/js/2.54ed5b3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boxoffice/static/js/2.54ed5b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a68841e8bdd95db5b3f",
+    "url": "/boxoffice/static/js/main.dc2f2bd0.chunk.js"
+  },
+  {
+    "revision": "2713fdea49f1beda18e0",
+    "url": "/boxoffice/static/js/runtime-main.0fb93b9d.js"
+  }
+]);
